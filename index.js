922f898c98847c1fd8faa7c7025352add230dfdd
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2017-03-13 16:20:36 +0100

    Taking port from env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
 
+var port = process.env.PORT || 3000;
+
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type' : 'text/plain'});
   res.end('OK\n');
-}).listen(80, '127.0.0.1');
+}).listen(port, '127.0.0.1');
