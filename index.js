fa82a3367fd3e31ad11286d4610f7b8bdc92f9c1
Steven Hein <steven.c.hein@gmail.com>
2017-03-14 16:05:01 +0100

    Add labeled and unlabeled functions

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-var port = process.env.PORT || 3000;
-var data;
+var port = process.env.PORT || 3000
+var githubBotUserId = process.env.GITHUB_BOT_USER_ID
+var giphyApiKey = process.env.GIPHY_API_KEY
+var cert = fs.readFileSync('./github.pem')
 
-var app = require('express')();
-var myParser = require("body-parser");
-app.use(myParser.json());
+var app = require('express')()
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
-var server = require('http').Server(app);
-server.listen(port);
+var server = require('http').Server(app)
+server.listen(port)
 
-app.post('/', function ({body}, res) {
-  console.log(body.action)
-  console.log(body.label.name)
-  console.log(body.pull_request.head.sha)
-  console.log(body.pull_request.head.ref)
-});
+var lollygag = require('@resmio/lollygag')
+
+app.post('/pull_request', function (req, res) {
+  if (body) {
+    const {action, pull_request, label} = req.body
+    switch (action) {
+      case 'labeled':
+        if (label.name === 'featuredeploy') {
+          lollygag.makeGithubFeatureDeployComments(cert, pull_request.head.ref, 'deploying feature...', githubBotUserId, giphyApiKey)
+          // RUN THE DEPLOY SCRIPT HERE
+        }
+        break
+      case 'unlabeled':
+        if (label.name === 'featuredeploy') {
+          lollygag.makeGithubFeatureDeployComments(cert, pull_request.head.ref, null, githubBotUserId, giphyApiKey)
+          // RUN THE DESTROY SCRIPT HERE
+        }
+        break
+    }
+  }
+})
