d3edd8daedaee9fa5b39369fb08a86c7ac8c3c55
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2017-03-14 15:24:13 +0100

    update

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-var http = require('http');
-
 var port = process.env.PORT || 3000;
 
-http.createServer(function(req, res) {
-    var body = '';
-    req.on('data', function(chunk) {
-      body += chunk;
-    });
-    req.on('end', function() {
-      var data = JSON.parse(body);
-      console.log(data.action)
-      console.log(data.label.name)
-      console.log(data.pull_request.head.sha)
-      console.log(data.pull_request.head.ref)
-      body = ''
-    });
+var app = require('express')();
+var server = require('http').Server(app);
+server.listen(port);
 
-  res.writeHead(200, {'Content-Type' : 'text/plain'});
-  res.end('OK\n');
+app.post('/', function (req, res) {
+  console.log(req)
+});
 
-}).listen(port, '0.0.0.0');
+//
+// var http = require('http');
+
+// var port = process.env.PORT || 3000;
+
+// http.createServer(function(req, res) {
+//     var body = '';
+//     req.on('data', function(chunk) {
+//       body += chunk;
+//     });
+//     req.on('end', function() {
+//       var data = JSON.parse(body);
+//       console.log(data.action)
+//       console.log(data.label.name)
+//       console.log(data.pull_request.head.sha)
+//       console.log(data.pull_request.head.ref)
+//       body = ''
+//     });
+
+//   res.writeHead(200, {'Content-Type' : 'text/plain'});
+//   res.end('OK\n');
+
+// }).listen(port, '0.0.0.0');
