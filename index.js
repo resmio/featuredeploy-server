c6bb072b3a2f80a52aaeee0d0b9cefafa6556eac
Steven Hein <steven.c.hein@gmail.com>
2017-03-14 11:36:06 +0100

    test with console log

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var http = require('http');
 
 var port = process.env.PORT || 3000;
+var data;
 
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type' : 'text/plain'});
+  console.log(req)
   res.end('OK\n');
 }).listen(port, '0.0.0.0');
