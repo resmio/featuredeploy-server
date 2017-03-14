96b6d700a689dd3988670e756947da74d427ecd9
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2017-03-14 15:38:40 +0100

    update

--- index.js
+++ index.js
@@ -7,36 +7,10 @@ app.use(myParser.json());
 var server = require('http').Server(app);
 server.listen(port);
 
-app.post('/', function (req, res) {
-  var body = req.body
-  console.log(body)
+app.post('/', function ({body}, res) {
   console.log(body.action)
   console.log(body.label.name)
   console.log(body.pull_request.head.sha)
   console.log(body.pull_request.head.ref)
 
 });
-
-//
-// var http = require('http');
-
-// var port = process.env.PORT || 3000;
-
-// http.createServer(function(req, res) {
-//     var body = '';
-//     req.on('data', function(chunk) {
-//       body += chunk;
-//     });
-//     req.on('end', function() {
-//       var data = JSON.parse(body);
-//       console.log(data.action)
-//       console.log(data.label.name)
-//       console.log(data.pull_request.head.sha)
-//       console.log(data.pull_request.head.ref)
-//       body = ''
-//     });
-
-//   res.writeHead(200, {'Content-Type' : 'text/plain'});
-//   res.end('OK\n');
-
-// }).listen(port, '0.0.0.0');
