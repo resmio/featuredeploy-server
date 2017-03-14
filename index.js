de01c6cc5feab2ebaff668928f754247ae3afaae
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2017-03-14 14:56:01 +0100

    Maybe print post data json

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var http = require('http');
+var qs = require('querystring');
 
 var port = process.env.PORT || 3000;
 
 http.createServer(function(req, res) {
+    var body = '';
+    req.on('data', function(chunk) {
+      body += chunk;
+    });
+    req.on('end', function() {
+      var data = qs.parse(body);
+      console.log(data)
+    });
+
   res.writeHead(200, {'Content-Type' : 'text/plain'});
   res.end('OK\n');
+
 }).listen(port, '0.0.0.0');
