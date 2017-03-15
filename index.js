222c41730fb2d2f202c09de0b00d82e1503afc97
Steven Hein <steven.c.hein@gmail.com>
2017-03-15 17:49:03 +0100

    trying it

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ app.use(bodyParser.json())
 var server = require('http').Server(app)
 server.listen(port)
 
+var integrationTools = require('./lib/integrationTools')
+
 app.post('/pull_request', function (req, res) {
-  const {action, pull_request, label, repo} = req.body
-  console.log(req.body)
+  const {action, pull_request, label, repo, installation} = req.body
   switch (action) {
     case 'labeled':
       if (label.name === 'featuredeploy') {
-
+        integrationTools.makeGithubFeatureDeployComments({
+          installationId: installation.id,
+          pullUrl: pull_request.url,
+          message: 'deploying'
+        })
       }
       break
     case 'unlabeled':
