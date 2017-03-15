54d922e1072d2adbd96596957152fe66bf9095e8
Steven Hein <steven.c.hein@gmail.com>
2017-03-15 17:40:10 +0100

    Add some stuff

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 var port = process.env.PORT || 3000
-var githubBotUserId = process.env.GITHUB_BOT_USER_ID
-var giphyApiKey = process.env.GIPHY_API_KEY
-var cert = process.env.GITHUB_PRIVATE_KEY
 
 var app = require('express')()
 var bodyParser = require('body-parser')
@@ -10,21 +7,18 @@ app.use(bodyParser.json())
 var server = require('http').Server(app)
 server.listen(port)
 
-var lollygag = require('@resmio/lollygag')
-
 app.post('/pull_request', function (req, res) {
   const {action, pull_request, label, repo} = req.body
+  console.log(req.body)
   switch (action) {
     case 'labeled':
       if (label.name === 'featuredeploy') {
-        lollygag.makeGithubFeatureDeployComments(1755, 15513, cert, repo.full_name, pull_request.head.ref, 'deploying feature...', githubBotUserId, giphyApiKey)
-        // RUN THE DEPLOY SCRIPT HERE
+
       }
       break
     case 'unlabeled':
       if (label.name === 'featuredeploy') {
-        lollygag.makeGithubFeatureDeployComments(1755, 15513, cert, repo.full_name, pull_request.head.ref, null, githubBotUserId, giphyApiKey)
-        // RUN THE DESTROY SCRIPT HERE
+
       }
       break
   }
