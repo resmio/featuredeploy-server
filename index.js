62ff6bc1416f1649049f59efd1791506b3adeabc
Steven Hein <steven.c.hein@gmail.com>
2017-03-16 11:41:21 +0100

    add in other stuff

--- index.js
+++ index.js
@@ -9,26 +9,58 @@ server.listen(port)
 
 var integrationTools = require('./lib/integrationTools')
 
-app.post('/pull_request', function (req, res) {
+const execFile = require('child_process').execFile;
+// call the featuredeploy script and passes the last stdout line as argument
+const featuredeploy = (args, callback) => {
+  execFile('featuredeploy', args, (error, stdout, stderr) => {
+    if (error) {
+      console.error('Error calling featuredeploy')
+      console.error(error)
+    } else {
+      stdout = stdout.trim()
+      let lastLine = stdout.substr(stdout.lastIndexOf('\n') + 1) // last line
+      callback(lastLine)
+    }
+  })
+}
+
+app.post('/pull_request', (req, res) => {
   const {action, pull_request, label, repo, installation} = req.body
   switch (action) {
     case 'labeled':
       if (label.name === 'featuredeploy') {
-        integrationTools.makeGithubFeatureDeployComments({
-          installationId: installation.id,
-          pullUrl: pull_request.url,
-          message: 'deploying'
+        featuredeploy(['deploy', pull_request.head.ref, pull_request.head.sha], (ip) => {
+          integrationTools.makeGithubFeatureDeployComments({
+            installationId: installation.id,
+            pullUrl: pull_request.url,
+            message: 'deploying to ' + ip
+          })
         })
       }
       break
     case 'unlabeled':
       if (label.name === 'featuredeploy') {
-        integrationTools.removeGithubFeatureDeploy({
-          installationId: installation.id,
-          pullUrl: pull_request.url
+        featuredeploy(['rmbranch', pull_request.head.ref], () => {
+          integrationTools.removeGithubFeatureDeploy({
+            installationId: installation.id,
+            pullUrl: pull_request.url
+          })
         })
       }
       break
   }
   res.sendStatus(200)
 })
+
+app.post('/destroy', (req, res) => {
+  const {full_name, branch} = req.body
+  integrationTools.removeGithubFeatureDeploy({
+    installationId: installation.id,
+    branch,
+    fullName: full_name
+  })
+})
+
+bla()
+
+function bla() { console.log('blah works') }
