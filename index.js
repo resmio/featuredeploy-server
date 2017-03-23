b2a52a37bb4a140a4bd9c5a29c3f00d526112f20
Steven Hein <steven.c.hein@gmail.com>
2017-03-23 11:15:33 +0100

    Add check for installation action

--- index.js
+++ index.js
@@ -23,41 +23,42 @@ const featuredeploy = (args, callback) => {
 }
 
 app.post('/pull_request', (req, res) => {
-  const {action, pull_request, label, repo, installation} = req.body
-  console.log(req.body)
-  switch (action) {
-    case 'labeled':
-      if (label.name === 'featuredeploy') {
-        integrationTools.makeGithubFeatureDeployComments({
-          installationId: installation.id,
-          pullUrl: pull_request.url,
-          message: 'building...'
-        })
-        featuredeploy(['deploy', pull_request.head.ref, pull_request.head.sha], () => false)
-      }
-      break
-    case 'unlabeled':
-      if (label.name === 'featuredeploy') {
-        integrationTools.removeGithubFeatureDeployComments({
-          installationId: installation.id,
-          pullUrl: pull_request.url
-        })
-        featuredeploy(['rmbranch', pull_request.head.ref], () => false)
-      }
-      break
-    case 'closed':
-      integrationTools.checkForFeatureDeployLabel({
-        installationId: installation.id,
-        labelsUrl: pull_request.issue_url + '/labels'
-      }).then((hasLabel) => {
-        if (hasLabel) {
+  const {action, pull_request, label, repository, installation} = req.body
+  if (installation && action && pull_request) { // we only want to concern ourselves with the intallation actions, not the user actions
+    switch (action) {
+      case 'labeled':
+        if (label.name === 'featuredeploy') {
+          integrationTools.makeGithubFeatureDeployComments({
+            installationId: installation.id,
+            pullUrl: pull_request.url,
+            message: 'building...'
+          })
+          featuredeploy(['deploy', pull_request.head.ref, pull_request.head.sha], () => false)
+        }
+        break
+      case 'unlabeled':
+        if (label.name === 'featuredeploy') {
           integrationTools.removeGithubFeatureDeployComments({
             installationId: installation.id,
             pullUrl: pull_request.url
           })
           featuredeploy(['rmbranch', pull_request.head.ref], () => false)
         }
-      })
+        break
+      case 'closed':
+        integrationTools.checkForFeatureDeployLabel({
+          installationId: installation.id,
+          labelsUrl: pull_request.issue_url + '/labels'
+        }).then((hasLabel) => {
+          if (hasLabel) {
+            integrationTools.removeGithubFeatureDeployComments({
+              installationId: installation.id,
+              pullUrl: pull_request.url
+            })
+            featuredeploy(['rmbranch', pull_request.head.ref], () => false)
+          }
+        })
+    }
   }
   res.sendStatus(200)
 })
